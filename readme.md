429098ce329d5b45e9f2c1f0fb85d4855fde447c
salahudinileye-cloud <salahudinileye@gmail.com>
2026-02-17 13:40:13 +0100

    Added project description to readme.md

---
+++ readme.md
@@ -0,0 +1,3 @@
+## GitHub flow exercise illustrating pull requests
+
+This exercise illustrates using GitHub flow workflow with pull requests.
--- src/main/java/com/example/githubflowexercise/GithubFlowExerciseApplication.java
+++ src/main/java/com/example/githubflowexercise/GithubFlowExerciseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubFlowExerciseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubFlowExerciseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubFlowExerciseApplication.class, args);
+    }
 
 }
