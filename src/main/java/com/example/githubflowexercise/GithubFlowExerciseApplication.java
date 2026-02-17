6d732653f5560ce92f39b39fc663ac6b62d2646d
salahudinileye-cloud <salahudinileye@gmail.com>
2026-02-17 13:21:40 +0100

    Initial commit

---
+++ src/main/java/com/example/githubflowexercise/GithubFlowExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubflowexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubFlowExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubFlowExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubflowexercise/GithubFlowExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubflowexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubFlowExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
